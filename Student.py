5be5c05f8dbad9c2c272dd8cbd241783f1342216
adityashettar <adityashettar01@gmail.com>
2025-11-10 11:45:31 +0530

    Correct argument access and improve input handling
    Fix argument handling and default values in Student.py

--- Student.py
+++ Student.py
@@ -1,1 +1,16 @@
+import sys
+          # Check if correct number of argument
+if len(sys.argy) == 3:
+  script_name = sys.argy[0]
+  name = sys.argy[1]
+  rollno = sys.argy[2]
+  print("User provided input values:")
+else:
+  script_name =sys.argy[0]
+  name = "Deepa"
+  rollno = "101"
+  print("No input given - using default values:")
 
+print ("Script Name:",script_name)
+print ("Student Name:", name)
+print ("Roll number:",rollno)
